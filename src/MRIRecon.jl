582438d7fc03b7f1fd0efede987942d0bb05580f
Felix Horger <felix.horger@kcl.ac.uk>
2022-02-08 15:30:10 +0000

    Added Roemer recon and improved RSOS

--- src/MRIRecon.jl
+++ src/MRIRecon.jl
@@ -1,10 +1,21 @@
 
 module MRIRecon
 
-	function root_sum_of_squares(signals::AbstractArray{<: T}, dim::Val{N}=Val(1)) where T <: Number where N
+	function root_sum_of_squares(signals::AbstractArray{<: Number}, dim::Val{N}=Val(1)) where N
 		result = dropdims(sum(abs2, signals; dims=N); dims=N)
 		@. result = sqrt(result)
 	end
 
+	function roemer(
+		signals::AbstractArray{<: Number},
+		sensitivity::AbstractArray{<: Number},
+		dim::Val{N}=Val(1)
+	) where N
+		dropdims(
+			sum(conj.(sensitivity) .* signals; dims=N) ./ sum(abs2, sensitivity; dims=N);
+			dims=N
+		)
+	end
+	# TODO: Smooth high res scan for computing coil profiles?
 end
 

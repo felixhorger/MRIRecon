bbc0a7b66355ba85b72b8a069c425e77c118ff47
Felix Horger <felix.horger@kcl.ac.uk>
2022-06-29 19:55:13 +0100

    Minor improvement

--- src/coil_combination.jl
+++ src/coil_combination.jl
@@ -1,5 +1,5 @@
 
-function root_sum_of_squares(signals::AbstractArray{<: Number}, dim::Val{N}=Val(1)) where N
+function root_sum_of_squares(signals::AbstractArray{<: Number}, dim::Val{N}) where N
 	result = dropdims(sum(abs2, signals; dims=N); dims=N)
 	@. result = sqrt(result)
 end

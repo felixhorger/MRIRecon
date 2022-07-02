439bf94e4f600142b51100d125453398340f7945
Felix Horger <felix.horger@kcl.ac.uk>
2022-07-03 00:02:07 +0100

    Improved function signature plan_psf

--- src/sense.jl
+++ src/sense.jl
@@ -1,8 +1,9 @@
 
 """
-	plan_PSF(F::LinearMap, M::LinearMap [, S::LinearMap])
+	plan_PSF([S::LinearMap,] F::LinearMap, M::LinearMap)
+	Matrices in the order in which they are applied
 
 """
 plan_psf(F::LinearMap, M::LinearMap) = F' * M * F
-plan_psf(F::LinearMap, M::LinearMap, S::LinearMap) = S' * F' * M * F * S
+plan_psf(S::LinearMap, F::LinearMap, M::LinearMap) = S' * F' * M * F * S
 

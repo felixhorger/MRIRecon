3e70c6ae1e3d7abf528474a69d246884c94f3f4e
Felix Horger <felix.horger@kcl.ac.uk>
2022-07-18 16:59:50 +0100

    Improved code regarding how dynamic imaging is handled. Solved bug, now readouts with lengths > 1 can be used. Function to find which indices are sampled at which "dynamic frame" Function to find the indices of the centre of an array

--- src/masking.jl
+++ src/masking.jl
@@ -1,20 +1,19 @@
 
 """
 	dense sampling mask
-	last element of shape is dynamic dimension, e.g. time
 """
 function sampling_mask(
+	indices::AbstractVector{<: NTuple{N, Integer}},
 	shape::NTuple{N, Integer},
-	indices::AbstractVector{<: NTuple{D, Integer}}
-) where {N, D}
-	@assert N == D + 1
-	b = zeros(Float64, shape)
-	n = shape[1]
+	num_dynamic::Integer
+) where N
+	b = zeros(Float64, 1, shape..., num_dynamic) # The one is for the readout axis
 	for (i, j) in enumerate(indices)
-		b[j..., mod1(i, n)] = 1
+		b[1, j..., mod1(i, num_dynamic)] = 1
 	end
 	return b
 end
+
 
 
 """
@@ -22,8 +21,8 @@ end
 	Spatial shape and indices must be sorted,
 """
 @generated function unsampled_indices(
-	shape::NTuple{N, Integer},
-	indices::AbstractVector{<: NTuple{N, Integer}}
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	shape::NTuple{N, Integer}
 ) where N
 	return quote
 		@assert length(indices) != 0
@@ -48,68 +47,83 @@ end
 	end
 end
 
+
+"""
+	One vector for each dynamic
+"""
+function split_sampling(
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	num_dynamic::Integer
+) where N
+
+	min_indices_per_dynamic = length(indices) ÷ num_dynamic
+	split_indices = [
+		Vector{NTuple{N, Int64}}(
+			undef,
+			min_indices_per_dynamic + (j > mod(length(indices), num_dynamic) ? 0 : 1)
+		)
+		for j in 1:num_dynamic
+	]
+	split_i = zeros(Int64, num_dynamic)
+	for i in eachindex(indices)
+		j = mod1(i, num_dynamic)
+		split_i[j] += 1
+		split_indices[j][split_i[j]] = indices[i]
+	end
+	return split_indices
+end
+
+
 """
 	a[spatial dims..., channels, dynamic]
 
 """
 function apply_sparse_mask!(a::Array{<: Number, N}, indices_to_mask::Vector{Vector{NTuple{D, Int64}}}) where {N, D}
-	@assert N == D + 2
+	@assert N == D + 3 # Readout, channels, dynamic
 	for j = 1:size(a, N)
-		@views @inbounds for i in indices_to_mask[j]
-			a[i..., :, j] .*= 0
+		@inbounds for i in indices_to_mask[j]
+			a[:, i..., :, j] .= 0
 		end
 	end
 	return a
 end
 
+
 """
 	Linear operator to perform masking efficiently (in place, sparse mask)
-	last element in shape is other dims e.g. dynamic
-	second last is channels
 """
 function plan_masking(
-	shape::NTuple{N, Integer},
-	indices::AbstractVector{<: NTuple{D, Integer}}
-) where {N, D}
-	@assert N == D + 2
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	target_shape::NTuple{M, Integer}
+) where {N, M}
+	@assert N == M - 3 # Readout, channels, dynamic
 
-	spatial_shape = shape[1:D]
-	# Note: channels = shape[N-1]
-	num_dynamic = shape[N]
+	# Get dimensions
+	shape = target_shape[2:N+1]
+	num_dynamic = target_shape[M]
 
-	# Get linear indices for the spatial dimensions
-	linear_indices = LinearIndices(spatial_shape)
-
-	# Split into frames
-	split_indices = [
-		Vector{NTuple{D, Int64}}(undef, length(indices) ÷ num_dynamic + 1)
-		for _ in 1:num_dynamic
-	]
-	readouts_per_dynamic = zeros(Int64, num_dynamic)
-	for i in eachindex(indices)
-		j = mod1(i, num_dynamic)
-		readouts_per_dynamic[j] += 1
-		split_indices[j][readouts_per_dynamic[j]] = indices[i]
-	end
+	# Split into dynamic frames
+	split_indices = split_sampling(indices, num_dynamic)
 
 	# Sort within each dynamic frame and find unsampled indices
-	indices_to_mask = Vector{Vector{NTuple{D, Int64}}}(undef, num_dynamic)
-	let by = (t::NTuple{D, Integer} -> linear_indices[t...])
+	indices_to_mask = Vector{Vector{NTuple{N, Int64}}}(undef, num_dynamic)
+	let
+		linear_indices = LinearIndices(shape)
+		by = (t::NTuple{N, Integer} -> linear_indices[t...])
 		@views for j = 1:num_dynamic
-			a_dynamic = split_indices[j][1:readouts_per_dynamic[j]]
-			sort!(a_dynamic; by)
-			indices_to_mask[j] = unsampled_indices(spatial_shape, a_dynamic)
+			sort!(split_indices[j]; by)
+			indices_to_mask[j] = unsampled_indices(split_indices[j], shape)
 		end
 	end
 
 	# Construct linear operator
 	MHM = LinearMap{ComplexF64}(
 		x::AbstractVector{<: Complex} -> begin
-			x_in_shape = reshape(x, shape)
+			x_in_shape = reshape(x, target_shape)
 			apply_sparse_mask!(x_in_shape, indices_to_mask)
 			x
 		end,
-		prod(shape),
+		prod(target_shape),
 		ishermitian=true,
 		issymmetric=true
 	)
@@ -121,20 +135,39 @@ end
 """
 	For kspace data
 	readout direction and channels must be first axes of a
-	dynamic dimension is the last, assumed mod(readout index, shape[N])
+	dynamic dimension is the last, assumed mod1(readout index, shape[N])
 """
 function sparse2dense(
 	a::AbstractArray{<: Number, 3},
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	shape::NTuple{N, Integer}
+) where N
+	# Not optimal performance, but better than copying code
+	# and also this shouldn't be a performance critical function
+	b = sparse2dense(a, indices, shape, 1)
+	b = dropdims(b; dims=N+3) # N does not include readout direction, channels and dynamic
+	return b
+end
+function sparse2dense(
+	a::AbstractArray{<: Number, 3},
+	indices::AbstractVector{<: NTuple{N, Integer}},
 	shape::NTuple{N, Integer},
-	indices::AbstractVector{<: NTuple{M, Integer}}
-) where {N, M}
-	@assert N == M + 1
+	num_dynamic::Integer
+) where N
 	@assert size(a, 3) == length(indices)
-	n = shape[N]
-	b = zeros(eltype(a), size(a, 1), shape[1:N-1]..., size(a, 2), n)
+	b = zeros(eltype(a), size(a, 1), shape..., size(a, 2), num_dynamic)
 	for (i, j) in enumerate(indices)
-		b[:, j..., :, mod1(i, n)] = a[:, :, i]
+		b[:, j..., :, mod1(i, num_dynamic)] = a[:, :, i]
 	end
 	return b
+end
+
+
+function centre_indices(shape::Integer, centre_size::Integer)
+	centre = shape ÷ 2
+	half = centre_size ÷ 2
+	lower = centre - half + 1
+	upper = centre + half + mod(centre_size, 2)
+	return lower:upper
 end
 
